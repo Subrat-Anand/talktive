1f88f7933f48e39fea6732ac4b86e521f931a421
Subrat-Anand <143004156+Subrat-Anand@users.noreply.github.com>
2026-01-15 20:04:22 +0530

    Destructure token from req.cookies in isAuth middleware

--- server/src/middleware/isAuth.js
+++ server/src/middleware/isAuth.js
@@ -3,7 +3,7 @@ const User = require("../models/user.model");
 
 const isAuth = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const {token} = req.cookies;
 
     if (!token) {
       return res.status(401).json({
